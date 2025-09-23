297b3f9e08d47770ebb6a49a3b971ee178116572
Lucas Geso <lucasgeso@live.com.ar>
2025-09-23 20:24:09 -0300

    Creación del menu utilizando Enum

--- interfaz/unlam/edu/ar/pb2/PruebaEstacionamiento.java
+++ interfaz/unlam/edu/ar/pb2/PruebaEstacionamiento.java
@@ -1,10 +1,51 @@
 package unlam.edu.ar.pb2;
 
+import java.util.Scanner;
+
 public class PruebaEstacionamiento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		MenuEstacionamiento menu[]= MenuEstacionamiento.values();
+		MenuEstacionamiento opcion;
+		
+		for(int i=0;i<menu.length;i++) {
+			System.out.println(menu[i]);
+		}
+		
+		System.out.println( "\n" + "Ingrese una de las opciones: ");
+		opcion = menu[teclado.nextInt()];
+		
+		switch(opcion) {
+		case INGRESO_VEHICULOS:
+			ingresarVehiculo();
+			break;
+		case UBICAR_VEHICULO:
+			ubicarVehiculo();
+			break;
+		case RETIRAR_VEHICULO:
+			returarVehiculo();
+			break;
+		default:
+			break;
+		}
+	}
 
+	private static void returarVehiculo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ubicarVehiculo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void ingresarVehiculo() {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
--- src/unlam/edu/ar/pb2/MenuEstacionamiento.java
+++ src/unlam/edu/ar/pb2/MenuEstacionamiento.java
@@ -2,4 +2,15 @@ package unlam.edu.ar.pb2;
 
 public enum MenuEstacionamiento {
 
+	INGRESO_VEHICULOS("0 - Ingresar un vehículo"),UBICAR_VEHICULO("1 - Buscar un vehículo"), RETIRAR_VEHICULO("2 - Retirar un vehículo"), SALIR("3 - Salir");
+	
+	private String texto;
+	
+	private MenuEstacionamiento(String textoInicial) {
+		this.texto=textoInicial;
+	}
+	
+	public String toString() {
+		return texto;
+	}
 }
